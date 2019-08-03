93c785f4d3b3ac1f824e2242006a680d152b2173
Andrew Ratnikov <ratnikov.am@gmail.com>
2019-08-03 15:47:42 +0300

    update structure

---
+++ src/components/App/index.tsx
@@ -0,0 +1,11 @@
+// Core
+import React from "react";
+
+// Styles
+import "./App.css";
+
+const App: React.FC = () => {
+  return <div className="App" />;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Core
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Styles
+import "./index.css";
+
+// Components
+import App from "./components/App";
+
+// Service workers
+import * as serviceWorker from "./serviceWorkers";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
