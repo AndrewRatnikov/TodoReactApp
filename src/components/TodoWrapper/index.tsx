1d3ce705f9a1506e6093489af21e4eb7a2865506
Andrew Ratnikov <ratnikov.am@gmail.com>
2019-09-02 21:54:25 +0300

    add TodoWrapper

--- src/components/TodoWrapper/index.tsx
+++ src/components/TodoWrapper/index.tsx
@@ -2,17 +2,19 @@
 import React, { FC } from "react";
 import styled from "styled-components";
 
+// Components
+
 const TodoSection = styled.section`
   min-width: 400px;
   min-height: 50px;
   background-color: #fff;
   margin: 10px;
-  border-radius: 1px;
+  border-radius: 3px;
   box-shadow: 1px 1px 10px 10px rgba(0, 0, 0, 0.1);
 `;
 
 const TodoWrapper: FC = () => {
-  return <TodoSection>Todo Wrapper</TodoSection>;
+  return <TodoSection>TODO</TodoSection>;
 };
 
 export default TodoWrapper;
